be34713781e4b325e9ea29b459c6b9d7bde8ae88
jongheonleee <qwefghnm1212@gmail.com>
2025-02-26 16:47:01 +0900

    [chore] 스프링부트 백엔드 설정

---
+++ sodam-backend/src/test/kotlin/com/backend/sodam/SodamApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.backend.sodam
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SodamApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
