b883a156a8336e5ee2aa272e8c89d1607eebfc89
jongheonleee <qwefghnm1212@gmail.com>
2025-02-26 17:36:27 +0900

    [chore] 초기 라우팅 연결 테스트

--- sodam-frontend/src/App.tsx
+++ sodam-frontend/src/App.tsx
@@ -1,25 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes, Navigate, Link} from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/articles">Articles</Link></li>
+        <li><Link to="/articles/new">Articles New</Link></li>
+        <li><Link to="/articles/edit">Articles New</Link></li>
+      </ul>
+
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/articles" element={<h1>Articles</h1>} />
+        <Route path="/articles/:id" element={<h1>Articles Detail</h1>} />
+        <Route path="/articles/new" element={<h1>Articles New Page</h1>} />
+        <Route path="/articles/edit/:id" element={<h1>Articles Edit Page</h1>} />
+        <Route path="/profile" element={<h1>Profile Page</h1>} />
+        <Route path="*" element={<Navigate replace to="/" />} />
+      </Routes>
+      </>
   );
 }
 
