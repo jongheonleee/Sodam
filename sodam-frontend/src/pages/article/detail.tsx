3f984e9217dd0535c50d3aad8e7bb7f7c0004de1
jongheonleee <qwefghnm1212@gmail.com>
2025-02-28 14:45:01 +0900

    [feat] 큰 틀 잡아두기

---
+++ sodam-frontend/src/pages/article/detail.tsx
@@ -0,0 +1,13 @@
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
+import ArticleDetail from "../../components/ArticleDetail";
+
+export default function ArticleDetailPage() {
+    return (
+        <>
+            <Header />
+            <ArticleDetail />
+            <Footer />
+        </>
+    )
+}
