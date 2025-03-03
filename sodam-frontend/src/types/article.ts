b3f0c83a61a5541ff8ca2563832fc1f6dceeecbf
jongheonleee <qwefghnm1212@gmail.com>
2025-03-03 11:18:13 +0900

    [feat] 게시글 요약 타입 정의(+이미지, 태그, 카테고리)

---
+++ sodam-frontend/src/types/article.ts
@@ -0,0 +1,34 @@
+// 게시글과 관련된 데이터 타입 정의
+// - 메인 페이지에서 보여줄 게시글 타입
+// - id, category, email, author, createdAt, userImage, title, summary, tags
+// - 추가로 정의해야 하는 타입 => userImage(이미지), tags(tag 배열)
+export interface Image {
+    url: string; // aws s3 저장된 url 경로
+    alt?: string; // 비고란
+}
+
+export interface Tag {
+    id: number;
+    articleId: number; // 연관된 게시글
+    content: string; // 태그 이름
+}
+
+export interface Category {
+    id: string;
+    topId: string; // 상위 카테고리 id
+    name: string;
+    ord: number; // 정렬 순서
+    validYN: number; // 사용 가능 여부(0, 1)
+}
+
+export interface ArticleSummary {
+    id: number;
+    category: Category; // category 타입 정의 해야함
+    email: string;
+    author: string;
+    createdAt: string;
+    userImage: Image; // 이미지 타입
+    title: string;
+    content: string;
+    tags: Tag[]; // 태그 배열
+}
