bdb6aed4c3d569c7a82a06f192b43c0363cd637c
jongheonleee <qwefghnm1212@gmail.com>
2025-03-07 17:09:31 +0900

    [chore] 목 데이터 쿼리문 추가

---
+++ sodam-backend/src/test/kotlin/com/backend/sodam/mocks/category/더미카테고리생성문.sql
@@ -0,0 +1,8 @@
+INSERT INTO `category` (`CATEGORY_ID`, `TOP_CATEGORY_ID`, `CATEGORY_NAME`, `CATEGORY_ORD`, `VALID_YN`, `CREATED_AT`, `CREATED_BY`, `MODIFIED_AT`, `MODIFIED_BY`)
+VALUES
+    ('cate_000', 'cate_000', '전체', 1, 0, NOW(), 'system', NOW(), 'system'),   -- '전체' 카테고리
+    ('cate_001', 'cate_000', '일상', 2, 0, NOW(), 'system', NOW(), 'system'),   -- '일상' 카테고리
+    ('cate_002', 'cate_000', '꿀 팁', 3, 0, NOW(), 'system', NOW(), 'system'),   -- '꿀 팁' 카테고리
+    ('cate_003', 'cate_000', 'IT 커리어', 4, 0, NOW(), 'system', NOW(), 'system'),  -- 'IT 커리어' 카테고리
+    ('cate_004', 'cate_000', '공부', 5, 0, NOW(), 'system', NOW(), 'system'),   -- '공부' 카테고리
+    ('cate_005', 'cate_000', '취미', 6, 0, NOW(), 'system', NOW(), 'system');  -- '취미' 카테고리
