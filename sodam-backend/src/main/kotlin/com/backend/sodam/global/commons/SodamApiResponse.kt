ffba64c941d50f1c58d2ab370d0f221bfec9273f
jongheonleee <qwefghnm1212@gmail.com>
2025-03-07 13:51:54 +0900

    [comment] 주석 추가

--- sodam-backend/src/main/kotlin/com/backend/sodam/global/commons/SodamApiResponse.kt
+++ sodam-backend/src/main/kotlin/com/backend/sodam/global/commons/SodamApiResponse.kt
@@ -7,8 +7,10 @@ data class SodamApiResponse<T>(
     val message: String?,
     val data: T?
 ) {
+    //  클래스 내부에서 정의되는 싱글턴 객체로, 자바의 static 멤버와 유사한 역할을 함
+    //  => 해당 부분을 'companion object' 로 적용한 이유는 팩토리 메서드로서 오브젝트를 생성하는 역할을 담당하기 때문
     companion object {
-        const val CODE_SUCCESS = "success"
+        private const val CODE_SUCCESS = "success"
 
         fun <T> ok(data: T) : SodamApiResponse<T> {
             return SodamApiResponse(true, CODE_SUCCESS, null, data)
