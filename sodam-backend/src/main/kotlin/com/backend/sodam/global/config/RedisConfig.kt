bd2ad862ee4cdeba397311a1d5acbdea39740f37
jongheonleee <qwefghnm1212@gmail.com>
2025-03-10 19:04:44 +0900

    [chore] 추적되지 않은 파일 전부 첨부

--- sodam-backend/src/main/kotlin/com/backend/sodam/global/config/RedisConfig.kt
+++ sodam-backend/src/main/kotlin/com/backend/sodam/global/config/RedisConfig.kt
@@ -1,4 +1,23 @@
 package com.backend.sodam.global.config
 
-class RedisConfig {
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
+
+@Configuration
+class RedisConfig(
+    @Value("\${spring.data.redis.host}")
+    val host: String,
+
+    @Value("\${spring.data.redis.port}")
+    val port: Int,
+) {
+
+    @Bean
+    fun redisConnectionFactory(): RedisConnectionFactory {
+        return LettuceConnectionFactory(host, port)
+    }
 }
--- sodam-backend/src/main/kotlin/com/backend/sodam/global/redis/RedisRepository.kt
+++ sodam-backend/src/main/kotlin/com/backend/sodam/global/redis/RedisRepository.kt
@@ -1,4 +1,25 @@
 package com.backend.sodam.global.redis
 
-class RedisRepository {
+import lombok.RequiredArgsConstructor
+import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.stereotype.Repository
+import java.time.Duration
+
+@Repository
+@RequiredArgsConstructor
+class RedisRepository(
+    private val redisTemplate: RedisTemplate<String, String>,
+) {
+
+    fun getValue(key: String): String? {
+        return redisTemplate.opsForValue().get(key)
+    }
+
+    fun setValue(key: String, value: String) {
+        redisTemplate.opsForValue().set(key, value)
+    }
+
+    fun setValueTtl(key: String, value: String, ttl: Duration) {
+        redisTemplate.opsForValue().set(key, value, ttl)
+    }
 }
