8b0c92c1c23be3582a404be278c96591626e0d3e
jongheonleee <qwefghnm1212@gmail.com>
2025-03-10 18:33:50 +0900

    [chore] 공통 예외 처리기 정의

---
+++ sodam-backend/src/main/kotlin/com/backend/sodam/global/commons/GlobalExceptionHandler.kt
@@ -0,0 +1,25 @@
+package com.backend.sodam.global.commons
+
+import lombok.extern.slf4j.Slf4j
+import org.slf4j.LoggerFactory
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestControllerAdvice
+
+
+// 참고 : https://cheese10yun.github.io/spring-guide-exception/
+@Slf4j
+@RestControllerAdvice
+class GlobalExceptionHandler {
+
+    private val log = LoggerFactory.getLogger(GlobalExceptionHandler::class.java)
+
+    // 공통 RuntimeException 핸들링
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(RuntimeException::class)
+    fun handleRuntimeException(e: RuntimeException): SodamApiResponse<*> {
+        log.error("error={}", e.message, e)
+        return SodamApiResponse.fail(ErrorCode.DEFAULT_ERROR, e.message.toString()) // 추후에 상태 코드 어디에서 처리할지 고민
+    }
+}
