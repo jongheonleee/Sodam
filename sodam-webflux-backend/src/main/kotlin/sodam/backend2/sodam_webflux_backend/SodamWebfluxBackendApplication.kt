671a3600ce7581cef5fb3b2966bc63da2ae1086b
jongheonleee <qwefghnm1212@gmail.com>
2025-05-06 18:32:41 +0900

    [chore] SpringWebflux 백엔드 환경 세팅

---
+++ sodam-webflux-backend/src/main/kotlin/sodam/backend2/sodam_webflux_backend/SodamWebfluxBackendApplication.kt
@@ -0,0 +1,11 @@
+package sodam.backend2.sodam_webflux_backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SodamWebfluxBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<SodamWebfluxBackendApplication>(*args)
+}
